faa2ed1ac464ba2e65c04f293864dec7758ca6f1
rayhan57 <rayhanb18@gmail.com>
2024-01-29 11:37:10 +0700

    add .env

--- server/index.js
+++ server/index.js
@@ -6,11 +6,9 @@ import {
   getAllStudents,
   updateStudent,
 } from "./src/controllers/studentsController.js";
-import dbConnection from "./src/database/dbConnection.js";
 
 const app = express();
 const port = 3000;
-const db = dbConnection();
 
 app
   .use(cors())
@@ -18,14 +16,6 @@ app
   .listen(port, () =>
     console.log(`Server running at http://localhost:${port}`)
   );
-
-db.connect((err) => {
-  if (err) {
-    console.log("Error connecting to MySQL...", err);
-  } else {
-    console.log("MySQL connected...");
-  }
-});
 
 app.get("/api/getAllStudents", getAllStudents);
 
--- server/src/database/dbConnection.js
+++ server/src/database/dbConnection.js
@@ -1,12 +1,26 @@
 import mysql from "mysql";
+import { config } from "dotenv";
+
+config();
 
 const dbConnection = () => {
-  return mysql.createConnection({
-    host: "localhost",
-    database: "react_mysql",
-    user: "root",
-    password: "",
+  const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+  const connection = mysql.createConnection({
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USERNAME,
+    password: DB_PASSWORD,
   });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL:", err);
+      return;
+    }
+    console.log("MySQL connected.");
+  });
+
+  return connection;
 };
 
 export default dbConnection;
